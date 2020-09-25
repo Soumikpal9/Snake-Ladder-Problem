70abee8c3b89b2f0716ed0b3b7913a8de44afb56
Soumikpal9 <soumikpal1997@gmail.com>
2020-09-25 10:58:03 +0530

    Added: Initial Position

--- src/com/capgemini/SnakeLadder.java
+++ src/com/capgemini/SnakeLadder.java
@@ -1,7 +1,12 @@
 package com.capgemini;
 
+//UC1
 public class SnakeLadder {
+	//Constants
+	public static final int NO_OF_PLAYERS = 1;
+	public static final int INITIAL_POSITION = 0;
 	public static void main(String[] args) {
-		System.out.println("Welcome to the game!!!");
+		System.out.println("Welcome to Snake Ladder Game!!!");
+		System.out.println("Player is at the starting position");
 	}
 }
